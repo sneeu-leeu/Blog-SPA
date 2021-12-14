729463173a88f0ed4744bf8fee3b5657ea13d651
sneeu-leeu <sa070197@gmail.com>
2021-12-14 14:10:45 +0200

    Create method to get6 recent comments from a post

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,13 @@
 class Post < ApplicationRecord
   belongs_to :user
+  has_many :comments
+  has_many :likes
 
   def update_post_counter(arg)
     user.update('post_counter', arg)
+  end
+
+  def recent_post_comments(limit = 5)
+    comments.order(created_at: :desc).limit(limit)
   end
 end
