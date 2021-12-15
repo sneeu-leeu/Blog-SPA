f90c40b3a8ebcf017e7a720c3e20aefa4d2eda0d
sneeu-leeu <sa070197@gmail.com>
2021-12-15 11:33:29 +0200

    intsall faker and set up seed.rb

--- Gemfile
+++ Gemfile
@@ -14,6 +14,7 @@ gem 'puma', '~> 5.0'
 gem 'sass-rails', '>= 6'
 gem 'turbolinks', '~> 5'
 gem 'webpacker', '~> 5.0'
+gem 'faker'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+10.times do
+  name = Faker::Name.first_name
+  bio_quote = Faker::Quotes::Shakespeare
+    user = User.create(
+      name: name,
+      photo: "https://randomuser.me/api/portraits/men/jpg",
+      bio: bio_quote
+    )
+    5.times do |i|
+      post = Post.create(
+        user_id: user.id,
+        title: 'Shakespear bish',
+        text: bio_quote)
+        7.times do |j|
+          comment = Comment.create(
+            user_id: user.id,
+            posts_id: post.id,
+            text: 'Thau Post art scheit beith a bit less lameth'
+          )
+        end
+    end
+end
